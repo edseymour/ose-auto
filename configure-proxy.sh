1816dd2a72275f870a8330ae58f469b92b63dfe7
Ed Seymour <eseymour@redhat.com>
2015-12-10 12:41:31 +0000

    updated configure-proxy.sh with sudo

--- configure-proxy.sh
+++ configure-proxy.sh
@@ -28,25 +28,25 @@ echo "       - /etc/sysconfig/docker"
 echo "*** Only proceed if you are absolutely sure that's what you want to do, CTRL-C to exit now"
 read -p "Press any key to continue"
 
-function install_prereqs
+function configure_proxy
 {
    fqdn=$1
    registry=$2
    # export the OSE_DEVICE variable
- scmd $ssh_user@$fqdn  "bash -c 'echo \"registry=${registry};  export registry\" > /etc/profile.d/registry.sh'"
- scmd $ssh_user@$fqdn  "bash -c 'echo \"HTTPS_PROXY_var=$HTTPS_PROXY_var;  export HTTPS_PROXY_var\" > /etc/profile.d/HTTPS_PROXY_var.sh'"
+ scmd $ssh_user@$fqdn  sudo "bash -c 'echo \"registry=${registry};  export registry\" > /etc/profile.d/registry.sh'"
+ scmd $ssh_user@$fqdn  sudo "bash -c 'echo \"HTTPS_PROXY_var=$HTTPS_PROXY_var;  export HTTPS_PROXY_var\" > /etc/profile.d/HTTPS_PROXY_var.sh'"
 
-  scmd $ssh_user@$fqdn "bash -c 'echo \"HTTP_PROXY_var=$HTTP_PROXY_var;  export HTTP_PROXY_var\" > /etc/profile.d/HTTP_PROXY_var.sh'"
+  scmd $ssh_user@$fqdn sudo "bash -c 'echo \"HTTP_PROXY_var=$HTTP_PROXY_var;  export HTTP_PROXY_var\" > /etc/profile.d/HTTP_PROXY_var.sh'"
 
-    scmd $ssh_user@$fqdn  "bash -c 'echo \"NO_PROXY_var="${NO_PROXY_var}";  export NO_PROXY_var\" > /etc/profile.d/NO_PROXY_var.sh'"
+    scmd $ssh_user@$fqdn  sudo "bash -c 'echo \"NO_PROXY_var="${NO_PROXY_var}";  export NO_PROXY_var\" > /etc/profile.d/NO_PROXY_var.sh'"
 
    scmd $ssh_user@$fqdn <<-\SSH
 
 # install pre-requisites
 dockerfile=/etc/sysconfig/docker
 # configure docker options
-sed -i "s/OPTIONS=.*/OPTIONS=\'--selinux-enabled --insecure-registry 172.30.0.0\/16\'/g" /etc/sysconfig/docker
-echo " ****** Start Host $fqdn ******* "
+sudo sed -i "s/OPTIONS=.*/OPTIONS=\'--selinux-enabled --insecure-registry 172.30.0.0\/16\'/g" /etc/sysconfig/docker
+echo " ****** Start Host $(hostname -f) ******* "
 echo " ** $HTTP_PROXY_var"
 echo " ** $HTTPS_PROXY_var"
 echo " ** $NO_PROXY_var"
@@ -55,17 +55,17 @@ echo " ** $NO_PROXY_var"
 
 if [ -f $dockerfile ]; then
         echo " ****** Docker file found:  setting $dockerfile"
-        grep -q 'HTTPS_PROXY' $dockerfile  && sed -i "s/.*HTTPS_PROXY.*/$HTTPS_PROXY_var/" $dockerfile || echo "$HTTPS_PROXY_var" >> $dockerfile
-        grep -q 'HTTP_PROXY' $dockerfile  && sed -i "s/.*HTTP_PROXY.*/$HTTP_PROXY_var/" $dockerfile || echo "$HTTP_PROXY_var" >> $dockerfile
-        grep -q 'NO_PROXY' $dockerfile  && sed -i "s/.*NO_PROXY.*/$NO_PROXY_var,$registry/" $dockerfile || echo "$NO_PROXY_var,$registry" >> $dockerfile
+        grep -q 'HTTPS_PROXY' $dockerfile  && sudo sed -i "s/.*HTTPS_PROXY.*/$HTTPS_PROXY_var/" $dockerfile || echo "$HTTPS_PROXY_var" | sudo tee -a $dockerfile
+        grep -q 'HTTP_PROXY' $dockerfile  && sudo sed -i "s/.*HTTP_PROXY.*/$HTTP_PROXY_var/" $dockerfile || echo "$HTTP_PROXY_var" sudo tee -a $dockerfile
+        grep -q 'NO_PROXY' $dockerfile  && sudo sed -i "s/.*NO_PROXY.*/$NO_PROXY_var,$registry/" $dockerfile || echo "$NO_PROXY_var,$registry" | tee -a $dockerfile
 fi
 ## replace node, api, masters
 for file in  /etc/sysconfig/atomic-openshift-*
 do
         echo " ****** setting proxy  $file ****** "
-         grep -q 'HTTPS_PROXY' $file  && sed -i "s/.*HTTPS_PROXY.*/$HTTPS_PROXY_var/g" $file || echo "$HTTPS_PROXY_var" >> $file
-        grep -q 'HTTP_PROXY' $file  && sed -i "s/.*HTTP_PROXY.*/$HTTP_PROXY_var/g" $file || echo "$HTTP_PROXY_var" >> $file
-        grep -q 'NO_PROXY' $file  && sed -i "s/.*NO_PROXY.*/$NO_PROXY_var/g" $file || echo "$NO_PROXY_var" >> $file
+        grep -q 'HTTPS_PROXY' $file  && sudo sed -i "s/.*HTTPS_PROXY.*/$HTTPS_PROXY_var/g" $file || echo "$HTTPS_PROXY_var" | sudo tee -a $file
+        grep -q 'HTTP_PROXY' $file  && sudo sed -i "s/.*HTTP_PROXY.*/$HTTP_PROXY_var/g" $file || echo "$HTTP_PROXY_var" | sudo tee -a $file
+        grep -q 'NO_PROXY' $file  && sudo sed -i "s/.*NO_PROXY.*/$NO_PROXY_var/g" $file || echo "$NO_PROXY_var" | sudo tee -a $file
 done
 echo " ****** done **********
 
@@ -76,21 +76,23 @@ exit
 SSH
 
 }
+
 echo babababai
 docker_registry=
 for node in $master
 do
    fqdn=$(gen_fqdn $node)
-    docker_registry=`scmd $fqdn oc get service -n default  | grep docker-registry | awk '{print $2}'`
+   docker_registry=$(scmd $fqdn sudo oc get service -n default  | grep docker-registry | awk '{print $2}')
    echo breaking
  break
 done
+
 echo $docker_registry
 for node in $hosts
 do
    fqdn=$(gen_fqdn $node)
 
-   install_prereqs $fqdn $docker_registry &
+   configure_proxy $fqdn $docker_registry &
 
 done
 
